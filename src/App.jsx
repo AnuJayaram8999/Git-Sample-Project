c8a94fdd734dfb60e66ebac52a0a420fe1af2247
AnuJayaram <anujayaram8999@gmail.com>
2024-12-07 17:07:58 +0530

    try branch commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edited Anu Home
+          Edited Anu Home Try Branch
         </p>
       </div>
       <p className="read-the-docs">
